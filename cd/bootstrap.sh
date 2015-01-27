b9401684fba2423714cc403dc01b440e01a59566
vifarcic <viktor.farcic@everis.com>
2015-01-27 19:45:16 +0100

    Removed workshop material

--- cd/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing Ansible..."
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install -y ansible
-cd /vagrant/ansible
