831c0987a1c7fbc709a0f3e5f2b0792c83270d38
Viktor <viktor@farcic.com>
2016-09-05 22:15:56 +0200

    Review

---
+++ jenkins-swarm/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+for i in 1 2 3; do
+
+    docker-machine create -d virtualbox swarm-$i
+
+    eval $(docker-machine env swarm-$i)
+
+    docker pull registry
+
+    docker pull jenkins:alpine
+
+    docker pull mongo
+
+    docker pull vfarcic/go-demo
+
+    docker pull vfarcic/docker-flow-proxy
+
+done
+
+for i in 1 2; do
+
+    docker-machine create -d virtualbox swarm-test-$i
+
+    eval $(docker-machine env swarm-test-$i)
+
+    docker pull vfarcic/jenkins-swarm-agent
+
+    docker pull registry
+
+    docker pull mongo
+
+done
